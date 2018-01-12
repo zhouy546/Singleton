409d6538a3147c76dec0fa0eb3b39ae21e240c71
zhouy546 <zhouyunqi546@gmail.com>
2018-01-12 17:26:49 +0800

    Initial commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("next serial:" + SerialNumberGenerator.Instance.NextSerial);
+            Console.WriteLine("next serial:" + SerialNumberGenerator.Instance.NextSerial);
+            Console.WriteLine("next serial:" + SerialNumberGenerator.Instance.NextSerial);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/SerialNumberGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SerialNumberGenerator
+    {
+        private static volatile SerialNumberGenerator instance;
+        public static SerialNumberGenerator Instance {
+            get {
+                if (instance == null) {
+                    instance = new SerialNumberGenerator();
+                }
+                return instance;
+            }
+        }
+
+        private int count;
+
+        private SerialNumberGenerator() { }
+        
+        public virtual int NextSerial
+        {
+            get {
+                return count++;
+            }
+
+        }
+    }
+}
